1e816fe62fe62f9b4305d3be945e5467fab73cc1
lishijiee <2424782003@qq.com>
2023-03-07 23:39:39 +0800

    配置文件，避免代码检查，重置页面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <P>{{name}}</P>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import {reactive,toRefs} from "vue"
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'home',
+  setup(){
+    const data = reactive({
+      name:"小红",
+      阿哥:20,
+      info:"<i>斜体</i>"
+    })
+    return data
   }
 }
 </script>
